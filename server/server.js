aa037d55cb958fb2f4626b952147f4df43c44184
Fabien Lebas <fabien.lebas@decathlon.com>
2020-03-17 00:05:33 +0300

    passport local first implementation

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy;
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const LocalStrategy = require("passport-local").Strategy;
+
+const users = require('./users.js');
 
 const port = process.env.PORT || 4000;
 
@@ -9,8 +11,7 @@ app.use(function(request, result, next) {
     const allowedOrigins = [
       'http://localhost:3000',
       'https://localhost:3000',
-      'http://localhost:4000',
-      'https://localhost:4000',
+      'http://localhost:4000'
      ];
     const origin = request.headers.origin;
     if (allowedOrigins.indexOf(origin) > -1) {
@@ -23,28 +24,67 @@ app.use(function(request, result, next) {
     next();
 });
 
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
+  function(username, password, cb) {
+    users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password !== password) { return cb(null, false);}
+      return cb(null, user);
     });
-  }
-));
+  }));
 
-app.post('/login', passport.authenticate('local', { successRedirect: '/',
-                                                    failureRedirect: '/login' }));
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function(user, callback) {
+  return callback(null, user.id);
+});
 
-app.get("/", function(request, result){
-  result.send("Welcome on the api root")
-})
+passport.deserializeUser(function(id, callback) {
+  return users.findById(id).then(user=>{
+    callback(null, user)
+  });
+});
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()); // for parsing application/json
+
+app.use(require("cookie-parser")());
+app.use(require('express-session')({ secret: 'gz§345GEZrtretz5432', resave: false, saveUninitialized: false }));
 
 app.listen(port, function () {
   console.log("Server http listening on port:" + port);
 });
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    console.log("logged in", req.body.username);
+    const userInfo = {
+      status: '200',
+      username: req.body.username
+    }
+    res.send(userInfo)
+  });
+
+
+app.get("/", function(request, result){
+  result.send("Welcome on the api root")
+})
