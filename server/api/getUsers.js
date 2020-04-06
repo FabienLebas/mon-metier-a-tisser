28f85b36d2bf7ab7031e5807308e405b87d7a4ff
Fabien Lebas <fabien.lebas@decathlon.com>
2020-04-06 23:47:36 +0300

    connection to Cloud SQL and passing users / checking for password

--- server/api/getUsers.js
+++ server/api/getUsers.js
@@ -4,12 +4,15 @@ function getUsers(){
   // const client = new Client({
   //   ssl: SSLValue
   // });
-  const client = new Client()
+  const client = new Client({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
   client.connect();
   return client.query("SELECT * FROM users")
   .then(result => {
-    console.log("users received : ");
-    console.log(result);
     client.end();
     return result.rows;
   })
--- server/users.js
+++ server/users.js
@@ -1,7 +1,16 @@
-var records = [
-    { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
-  , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
-];
+// var records = [
+//     { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
+//   , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
+// ];
+
+const getUsers = require("./api/getUsers.js");
+let records = [];
+getUsers()
+.then(result => {
+  console.log("j'ai récupéré les users : ");
+  console.log(result);
+  records = result;
+})
 
 exports.findById = function(id, cb) {
   process.nextTick(function() {
@@ -15,10 +24,15 @@ exports.findById = function(id, cb) {
 }
 
 exports.findByUsername = function(username, cb) {
+  console.log(`j'ai recu ${username}`);
   process.nextTick(function() {
+    console.log("records : ");
+    console.log(records);
     for (var i = 0, len = records.length; i < len; i++) {
+      console.log(`j'en suis à ${i} ${records[i].username} a comparer avec ${username}`);
       var record = records[i];
       if (record.username === username) {
+        console.log(`j'ai trouve ${record.username}`);
         return cb(null, record);
       }
     }
